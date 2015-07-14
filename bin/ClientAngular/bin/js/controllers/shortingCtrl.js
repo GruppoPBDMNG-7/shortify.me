fd09d6c2884550bc3230f9935d3a3821efadf7b8
Giovanni <giovanni.montenegrit@gmail.com>
2015-07-14 15:42:47 +0200

    aggiunto: blacklist per lingua e alcuni domini localizzazione geografica algoritmo dello shorturl

---
+++ bin/ClientAngular/bin/js/controllers/shortingCtrl.js
@@ -0,0 +1,62 @@
+app.controller('shortingCtrl', function($scope, $http, $timeout) {
+    
+    $scope.done = false;
+    $scope.error = false;
+    
+    $scope.customURLView = false;
+    $scope.textCustomURL = "Make your custom URL";
+    
+    /*
+     * Converte un url in uno short e memorizza i risultati nella variabile shorturl.
+     * Viene effettuata una richiesta in POST al server che si occupa della conversione.
+     */
+    $scope.convert = function(url, text) { 
+        if (url != "" && url !=  null) {
+            $scope.error = false;
+
+            //controllo per far scomparire il risultato di una conversione precedente
+            if($scope.done) {
+                document
+                    .getElementById("resultDiv")
+                    .setAttribute("class", "alert alert-success centeredText animated fadeOut");  
+            }
+
+            //timeout per far concludere l'animazione
+            $timeout(function() {
+
+                //richiesta in post, il secondo argomento è il json che viene trasferito
+                $http.post("http://localhost:4567/api/v1/convert", {longurl: url, customText: text})
+                .success(function(response) { 
+
+                    $scope.shorturl = response.shortUrl;
+
+                    //spostamento verso l'alto del container principale
+                    //document.getElementById("container").setAttribute("class", "animated fadeOutUpBig");
+
+                    //risultato con animazione
+                    document.getElementById("resultDiv").setAttribute("class", "alert alert-success centeredText animated fadeIn");  
+
+                    $scope.done = true;
+                })
+                .error(function() {
+                    document.getElementById("errorDiv").setAttribute("class", "alert alert-danger centeredText animated fadeIn");  
+                    $scope.error = true;
+                });
+            }, 1000);
+        }
+
+    }
+    
+    $scope.showCustomURLView = function() {
+        if(!$scope.customURLView) {
+            $scope.customURLView = true;
+            $scope.textCustomURL = "Shortify an URL";
+            document.getElementById("customURLDiv").setAttribute("class", "centeredText animated fadeIn");  
+        } else {
+            $scope.customURLView = false;
+            $scope.textCustomURL = "Make your custom URL";
+            document.getElementById("longURLDiv").setAttribute("class", "centeredText animated fadeIn");  
+        }
+    }
+});
+
