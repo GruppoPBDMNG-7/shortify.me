d38f1be3ab86cbb279e1f698f3953fcfa7eaff7d
aiovine <a.iovine1@studenti.uniba.it>
2015-07-15 15:48:00 +0200

    Fixato Statistics

--- ServerJava/src/me/shortify/dao/Statistics.java
+++ ServerJava/src/me/shortify/dao/Statistics.java
@@ -1,37 +1,87 @@
 package me.shortify.dao;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Map;
+import java.util.TimeZone;
+
+import org.json.JSONObject;
 
 public class Statistics {
-	private Map<String, Integer> countryCounters;
-	private Map<Date, Integer> dayCounters;
-	private Map<Date, Integer> hourCounters;
-	private int uniqueCounter;
+	private String shortUrl;
+	private Map<String, Long> countryCounters;
+	private Map<Date, Long> dayCounters;
+	private Map<Date, Long> hourCounters;
+	private long uniqueCounter;
 	
 	
-	public Statistics(Map<String, Integer> countryCounters,
-			Map<Date, Integer> dayCounters,
-			Map<Date, Integer> hourCounters, int uniqueCounter) {
+	public Statistics(String shortUrl, Map<String, Long> countryCounters,
+			Map<Date, Long> dayCounters,
+			Map<Date, Long> hourCounters, long uniqueCounter) {
+		this.shortUrl = shortUrl;
 		this.countryCounters = countryCounters;
 		this.hourCounters = hourCounters;
 		this.dayCounters = dayCounters;
 		this.uniqueCounter = uniqueCounter;
 	}
 	
-	public Map<String, Integer> getCountryCounters() {
+	public Map<String, Long> getCountryCounters() {
 		return countryCounters;
 	}
 	
-	public Map<Date, Integer> getDayCounters() {
+	public Map<Date, Long> getDayCounters() {
 		return dayCounters;
 	}
 	
-	public Map<Date, Integer> getHourCounters() {
+	public Map<Date, Long> getHourCounters() {
 		return hourCounters;
 	}
 	
-	public int getUniqueViews() {
+	public long getUniqueViews() {
 		return uniqueCounter;
+	}
+	
+	public String getShortUrl() {
+		return shortUrl;
+	}
+	
+	public JSONObject toJson() {
+		JSONObject json = new JSONObject();
+		json.put("shortUrl", shortUrl);
+		
+		JSONObject countryJS = new JSONObject();
+		for (Map.Entry<String, Long> e : countryCounters.entrySet()) {
+			countryJS.put(e.getKey(), e.getValue());
+		}
+		json.put("countryCounters", countryJS);
+		
+		JSONObject dayJS = new JSONObject();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		for (Map.Entry<Date, Long> e : dayCounters.entrySet()) {
+			Calendar c = new GregorianCalendar();
+			c.setTime(e.getKey());
+			c.setTimeZone(TimeZone.getTimeZone("UTC"));
+			sdf.setCalendar(c);
+			String date = sdf.format(c.getTime());		
+			dayJS.put(date, e.getValue());
+		}
+		json.put("dayCounters", dayJS);
+		
+		sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+		JSONObject hourJS = new JSONObject();
+		for (Map.Entry<Date, Long> e : hourCounters.entrySet()) {
+			Calendar c = new GregorianCalendar();
+			c.setTime(e.getKey());
+			c.setTimeZone(TimeZone.getTimeZone("UTC"));
+			sdf.setCalendar(c);
+			String date = sdf.format(c.getTime());			
+			hourJS.put(date, e.getValue());
+		}
+		json.put("hourCounters", hourJS);
+		
+		json.put("uniqueCounter", uniqueCounter);
+		
+		return json;
 	}
 }
