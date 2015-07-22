a0c4ed75572cc6f31be2e8b1cb5c953a8e60ed80
lovascio <cosimolovascio@hotmail.it>
2015-07-22 12:10:11 +0200

    Fix messaggio di errore

--- ServerJava/src/me/shortify/sparkserver/exception/Error.java
+++ ServerJava/src/me/shortify/sparkserver/exception/Error.java
@@ -4,11 +4,11 @@ import org.json.JSONObject;
 
 public class Error {
 	
-	public static final String URL_EXISTS = "The custom text specified alredy exists in the database. Try with another one.";
-	public static final String BAD_URL = "The URL specified refers to not safe web site. Try with another URL.";
+	public static final String URL_EXISTS = "The custom text specified already exists in the database. Try with another one.";
+	public static final String BAD_URL = "The URL specified refers to a not safe website. Try with another URL.";
 	public static final String BAD_CUSTOM_URL = "The custom text specified is not accepted.";
-	public static final String SHORT_URL_NOT_EXISTS = "The short URL specified does not exist";
-	public static final String URL_NOT_SPECIFIED = "You need to insert a short URL";
+	public static final String SHORT_URL_NOT_EXISTS = "The short URL specified does not exist.";
+	public static final String URL_NOT_SPECIFIED = "You need to insert a short URL.";
 	
 	private final String ERROR = "error";
 	
