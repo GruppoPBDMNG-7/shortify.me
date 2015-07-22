7849c54b4ac0494b1f39f17b90f97fca4ea3904d
lovascio <cosimolovascio@hotmail.it>
2015-07-22 12:10:43 +0200

    Piccole modifiche al client

--- ClientAngular/js/controllers/shortingCtrl.js
+++ ClientAngular/js/controllers/shortingCtrl.js
@@ -13,47 +13,50 @@ app.controller('shortingCtrl', function($scope, $rootScope, $http, $timeout) {
      */
     $scope.convert = function(url, text) {
         
-        $scope.showSpinner = true;
-        
-        if (url != "" && url !=  null) {
-            $rootScope.error = false;
-            $rootScope.notStatisticsView = true;
+        if(!$scope.showSpinner) {           
+            $scope.showSpinner = true;
 
-            //controllo per far scomparire il risultato di una conversione precedente
-            if($scope.done) {
-                document
-                    .getElementById("resultDiv")
-                    .setAttribute("class", "alert alert-success centeredText animated fadeOut");  
-            }
+            if (url != "" && url !=  null) {
 
-            //timeout per far concludere l'animazione
-            $timeout(function() {
-                
-                //richiesta in post, il secondo argomento è il json che viene trasferito
-                $http.post("http://localhost:4567/api/v1/convert", {longurl: url, customText: text})
-                .success(function(response) { 
+                $rootScope.error = false;
+                $rootScope.notStatisticsView = true;
 
-                    $scope.shorturl = response.shortUrl;
+                //controllo per far scomparire il risultato di una conversione precedente
+                if($scope.done) {
+                    document
+                        .getElementById("resultDiv")
+                        .setAttribute("class", "alert alert-success centeredText animated fadeOut");  
+                }
 
-                    //spostamento verso l'alto del container principale
-                    //document.getElementById("container").setAttribute("class", "animated fadeOutUpBig");
+                //timeout per far concludere l'animazione
+                $timeout(function() {
 
-                    //risultato con animazione
-                    document.getElementById("resultDiv").setAttribute("class", "alert alert-success centeredText animated fadeIn");  
-                    
+                    //richiesta in post, il secondo argomento è il json che viene trasferito
+                    $http.post("http://localhost:4567/api/v1/convert", {longurl: url, customText: text})
+                        .success(function(response) { 
+
+                            $scope.shorturl = response.shortUrl;
+
+                            //spostamento verso l'alto del container principale
+                            //document.getElementById("container").setAttribute("class", "animated fadeOutUpBig");
+
+                            //risultato con animazione
+                            document.getElementById("resultDiv").setAttribute("class", "alert alert-success centeredText animated fadeIn");  
+
+                            $scope.showSpinner = false;
+                            $rootScope.done = true;
+                        })
+                        .error(function(response) {
+                            $rootScope.textError = response.error;
+                            document.getElementById("errorDiv").setAttribute("class", "alert alert-danger centeredText animated fadeIn");  
+                            $scope.showSpinner = false;
+                            $rootScope.error = true;
+                        });
+                    }, 1000);
+
+                } else {
                     $scope.showSpinner = false;
-                    $rootScope.done = true;
-                })
-                .error(function(response) {
-                    $rootScope.textError = response.error;
-                    document.getElementById("errorDiv").setAttribute("class", "alert alert-danger centeredText animated fadeIn");  
-                    $scope.showSpinner = false;
-                    $rootScope.error = true;
-                });
-            }, 1000);
-            
-        } else {
-            $scope.showSpinner = false;
+                }
         }
     }
     
